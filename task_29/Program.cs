723c5730480694796be276548a3ead44fa661a83
Dmitriy <dmitrypolyakovd@gmail.com>
2023-04-25 13:02:54 +0300

    массив чисел

---
+++ task_29/Program.cs
@@ -0,0 +1,10 @@
+﻿ushort[] array = new ushort[8];
+Random random = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = (ushort)random.Next(0, 1000);
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
+
