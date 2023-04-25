2c6c5945763512e4e8829bcdeac3e80b1a6c3ba9
Dmitriy <dmitrypolyakovd@gmail.com>
2023-04-25 12:49:52 +0300

    сумма цифр

---
+++ task_27/Program.cs
@@ -0,0 +1,27 @@
+﻿int number = ReadInt("number");
+
+int ReadInt(string argument)
+{
+    int n;
+    bool checkingNumber;
+
+    do
+    {
+        Console.Write($"Input a {argument}: ");
+        
+        checkingNumber = int.TryParse(Console.ReadLine(), out n);
+    }
+    while (!checkingNumber);
+    return n;
+}
+
+int sum = 0;
+int temp = number;
+while (temp > 0)
+{
+    int digit = temp % 10;
+    sum += digit;
+    temp /= 10;
+}
+
+Console.WriteLine($"The sum of digits in {number} is {sum}.");
